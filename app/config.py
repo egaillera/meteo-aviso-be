d10cccff83f3892157c99745e2840e2ddd50a3e6
egaillera <egaillera@gmail.com>
2017-03-19 23:19:23 +0100

    Changes database user for the app

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ class Configuration(object):
 	APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
 	DEBUG=True
 	#SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/measures.db' % APPLICATION_DIR
-	SQLALCHEMY_DATABASE_URI ='postgresql://postgres:mmm_dba1@meteo-aviso-postgres:5432/meteo' 
+	SQLALCHEMY_DATABASE_URI ='postgresql://meteo:mmm_dba1@meteo-aviso-postgres:5432/meteo' 
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
