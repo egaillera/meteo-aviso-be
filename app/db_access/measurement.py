b2f097f15f1d819ed64cc4263d6d0d0d813aacbc
egaillera <egaillera@gmail.com>
2018-02-28 13:06:13 +0100

    Added last_measurement endpoint

--- app/db_access/measurement.py
+++ app/db_access/measurement.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm.exc import NoResultFound
 from models import *
 from util.distance import get_closest_station
 
-
+# TODO: check errors in query
 def get_closest_measurement(lat,lon):
 	
 	app.logger.info("---> get_closest_measurement()")
@@ -14,6 +14,9 @@ def get_closest_measurement(lat,lon):
 	# Get the closest one
 	closest_station = get_closest_station(active_stations,lat,lon)
 	
+	return get_last_measurement(closest_station.code)
+	
+	'''
 	# Get the most recent measurement 
 	data = Measurement.query.filter(Measurement.station==closest_station.code).\
 	                   order_by(Measurement.date_created.desc()).first().\
@@ -23,6 +26,33 @@ def get_closest_measurement(lat,lon):
 	data['name'] = closest_station.name
 	
 	return data
+	'''	
+
+def get_last_measurement(station_code):
+	
+	app.logger.info("---> get_last_measurement()")
+	
+	data = None
+	
+	# Get the most recent measurement
+	try:
+		m = Measurement.query.filter(Measurement.station==station_code).\
+		                                order_by(Measurement.date_created.desc()).first()                         
+		if m != None:
+			data = m.serialize 
+		
+		# Add station name
+		data['name'] = Station.query.filter(Station.code == station_code).one().name
+		
+	
+	except NoResultFound:
+		data = None
+	
+	return data
+	
+	
+	
+	
 
 
 
--- app/views.py
+++ app/views.py
@@ -27,6 +27,13 @@ def closest_station():
 	
 	return jsonify(db_access.measurement.get_closest_measurement(lat,lon))
 	
+@app.route('/last_measurement/<station_code>')
+def last_measurement(station_code):
+	""" Return last measurement of a specific station """
+	
+	result = db_access.measurement.get_last_measurement(station_code)
+	return jsonify(result) if result != None else "[]"
+	
 @app.route('/token', methods=['POST'])
 def save_token():
 	"""Save the token to the user table in the database"""
