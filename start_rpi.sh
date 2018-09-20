9e80d0009446e5fb7b908c456bbd31243aaf6d36
egaillera <egaillera@gmail.com>
2018-09-20 21:58:08 +0200

    Create base images for deploy sw changes faster

---
+++ start_rpi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+set -e 
+
+if [ "$ENV" = 'COLLECTOR' ]; then 
+	while true 
+	do 
+	   echo "Running Collection Daemon"
+	   python "/home/meteo/meteo-aviso-be/app/scripts/collect_data.py" 
+	   sleep 900
+	done
+	
+else 
+	echo "Running Application Server" 
+	exec uwsgi --plugin /usr/lib/uwsgi/plugins/http_plugin.so --plugin /usr/lib/uwsgi/plugins/python3_plugin.so --http 0.0.0.0:9090 --wsgi-file /home/meteo/meteo-aviso-be/app/main.py --callable app --stats 0.0.0.0:9091
+fi
